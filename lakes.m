273bba3a067f14d9fc860b1d42405360f0408373
Paul Boone <narcissus.pong@gmail.com>
2017-03-16 15:17:40 -0400

    addl cleanup

--- lakes.m
+++ lakes.m
@@ -44,8 +44,10 @@ lake.dem_filled = NaN;
 lake.outlet.x = 3.523578530974025e+05;
 lake.outlet.y = 1.612583134776515e+06;
 lake.outlet.z = NaN; % calculate after gaussian filter is applied, in case it changes
-lake.outlet.index = coord2ind(lake.dem, lake.outlet.x, lake.outlet.y)
+lake.outlet.index = coord2ind(lake.dem, lake.outlet.x, lake.outlet.y);
 lake.sediment_depth = 1.38;
+
+%% prep dem file GRIDobj
 
 % apply gaussian filter
 hsize = 10;
@@ -63,6 +65,8 @@ topo.flow.direction = FLOWobj(lake.dem_filled);
 % topo.flow.acceleration = flowacc(topo.flow.direction)
 topo.gradient = gradient8(lake.dem);
 
+%% determine lake size
+
 lake_gradient_threshold = 0.03;
 lake_depth = 1;
 d = drainagebasins(topo.flow.direction, lake.outlet.index);
@@ -78,91 +82,7 @@ topo.lake_filter(topo.drainage_basin_filter == 1 & ...
 num_lake_cells = length(find(topo.lake_filter == 1));
 num_basin_cells = length(find(topo.drainage_basin_filter == 1)) - num_lake_cells;
 
+%% calculate sediment volume from lake size
+
 sediment_volume = lake.sediment_depth * num_lake_cells * topo.cell_area;
-
-
-% Al=length(find(lk==1));
-% % Aw=A.Z(IX)-Al
-% Aw=nansum(B.Z(B.Z==1 & lk~=1))
-% pa=DEM.cellsize*DEM.cellsize;
-% %%
-% D=Dv(i);
-% Vemp(i)=D*Al*pa;
-% Vemp_u(i)=Vemp(i)/2; %the case of tringular cs rather than rect
-
-
-
-%
-% %%flow direction object
-% FD = FLOWobj(DEMf);
-%
-% %%flow acc
-% A = flowacc(FD);
-% G=gradient8(DEM);
-%
-% %%compute drainage basin for this outlet
-% x=xv(i);
-% y=yv(i);
-% IX=coord2ind(DEM,x,y);%get index of outlet
-%
-% zo=DEM.Z(IX);%outlet elev
-% B=drainagebasins(FD,IX);%get a binary grid of basin cells;
-% Bv{i}=B;
-%
-% %%detect lake
-% lk=DEM.Z*0;
-% lk(B.Z==1 & G.Z<0.03 & DEM.Z<zo+1)=1;
-% lkv{i}=lk;
-% %
-% Zb=DEM.Z*NaN;
-% Zb(B.Z==1)=DEM.Z(B.Z==1);
-% %
-
-% imagesc(Zb);
-% axis equal
-% shg
-% %     pause
-%
-% %%vol of sed in time frame
-% Al=length(find(lk==1));
-% % Aw=A.Z(IX)-Al
-% Aw=nansum(B.Z(B.Z==1 & lk~=1))
-% pa=DEM.cellsize*DEM.cellsize;
-% %%
-% D=Dv(i);
-% Vemp(i)=D*Al*pa;
-% Vemp_u(i)=Vemp(i)/2; %the case of tringular cs rather than rect
-
-
-
-
-
-
-
-
-
-
-
-
-% imagesc(lake.dem)
-% colorbar
-% disp(lake.dem.Z(lake.outlet.index))
-% dem1 = lake.dem
-% % initial_colormap = colormap
-%
-% pause
-%
-% figure
-%
-%
-% % DEMs.Z=imfilter(DEM.Z, h, 'replicate');
-% % DEM=DEMs;
-% disp(lake.dem.Z(lake.outlet.index))
-% imagesc(lake.dem)
-% % colormap(initial_colormap);
-% colorbar
-%
-% dem2 = lake.dem
-
-
-% DEM=DEMs;
+disp(sediment_volume);
