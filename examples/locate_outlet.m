ff80d6e7a8b1c064c07aa3278cb3f46e10d44108
Paul Boone <narcissus.pong@gmail.com>
2017-03-29 10:16:27 -0400

    convert locate_outlet into a convenient function so we can use it easily to evaluate lakes

--- examples/locate_outlet.m
+++ examples/locate_outlet.m
@@ -1,21 +1,23 @@
-close all, clear all, clc;
 
-project_dir = '/Users/pboone/Dropbox/Projects/Classes/GEOL 2049/project-lake-sediment-analysis/examples/';
-l = Lake();
-l.load_from_geotiff([project_dir, 'clipped_jan.tif'], NaN, NaN);
 
-figure;
-imagesc(l.dem_filled);
+function lake = locate_outlet(geotiff_path)
+  lake = Lake();
+  lake.load_from_geotiff(geotiff_path, NaN, NaN);
 
-hold on;
+  figure;
+  imagesc(lake.dem_filled);
 
-green = cat(3, zeros(size(l.dem.Z)), 0.5*ones(size(l.dem.Z)), zeros(size(l.dem.Z)));
-h = imshow(green, 'XData', l.dem.georef.SpatialRef.XWorldLimits, 'YData', flip(l.dem.georef.SpatialRef.YWorldLimits));
-plot(l.outlet.x,l.outlet.y, 'r+','LineWidth',5);
+  hold on;
 
-hold off;
+  green = cat(3, zeros(size(lake.dem.Z)), 0.5*ones(size(lake.dem.Z)), zeros(size(lake.dem.Z)));
+  h = imshow(green, 'XData', lake.dem.georef.SpatialRef.XWorldLimits, ...
+                    'YData', flip(lake.dem.georef.SpatialRef.YWorldLimits));
+  plot(lake.outlet.x,lake.outlet.y, 'r+','LineWidth',5);
 
-set(gca, 'YDir', 'normal');
-set(gca, 'Visible', 'on');
+  hold off;
 
-set(h, 'AlphaData', l.lake_filter) 
+  set(gca, 'YDir', 'normal');
+  set(gca, 'Visible', 'on');
+
+  set(h, 'AlphaData', lake.lake_filter) 
+end
