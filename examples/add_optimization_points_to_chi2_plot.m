129bca346cf14b64d6a5a2f1ea62c61a8ae8cd5f
Paul Boone <narcissus.pong@gmail.com>
2017-04-06 16:24:01 -0400

    replace lengthy point text with indices and a legend

--- examples/add_optimization_points_to_chi2_plot.m
+++ examples/add_optimization_points_to_chi2_plot.m
@@ -3,10 +3,14 @@ function add_optimization_points_to_chi2_plot(mins, chi2)
 
   plot(mins(:,1), mins(:,2), 'o', 'MarkerEdgeColor', 'r', 'MarkerFaceColor', 'r');
 
+  legend_labels = strings(size(mins,1));
   for i = 1:size(mins,1)
-    text_label = char(strcat(' - \chi= ', string(chi2(i)),' @ (', string(mins(i,1)), ', ', string(mins(i,2)),')'));
+    legend_labels(i) = char(strcat(num2str(i),') \chi= ', num2str(chi2(i),'%4.3e'),' @ (', num2str(mins(i,1)), ', ', num2str(mins(i,2)),')'));
+    text_label = strcat(' - (', num2str(i), ')');
     text(mins(i,1), mins(i,2),text_label);
   end
+  
+  legend(strjoin(legend_labels, '\n'),'Location','southoutside');
 
   hold off;
 end
